05931b5dcf0db1847edb3dc4919ad462d6c0f326
dtigranyan <dtigranyan1980@gmail.com>
2024-10-20 14:07:52 +0400

    Created news

---
+++ final.js
@@ -0,0 +1,10 @@
+let f1s = document.getElementById('f1');
+let f2s = document.getElementById('f2');
+let fs = document.getElementById('f');
+
+f1s.addEventListener('click', () => {
+    fs.style.backgroundImage = "url('styles/images/how-smoked-meat-can-be-kept-for-a-long-time-2_480x480.webp')"
+})
+f2s.addEventListener('click', () => {
+    fs.style.backgroundImage = "url('styles/images/smoked-pork-belly-6.jpg')"
+})
